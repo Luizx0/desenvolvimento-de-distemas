774a3dd4b36f32e50f7e48de51a0acdc84941449
Luizx0 <Loue0982@gmail.com>
2026-03-10 22:37:32 -0300

    feat: tarefa CliParametros em C#

---
+++ Programa console que recebe parametros e os devolve em dobro em C#/CliParametros/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int quantidade = args.Length;
+        int dobro = quantidade * 2;
+
+        Console.WriteLine($"Quantidade de parâmetros recebidos: {quantidade}");
+        Console.WriteLine($"Dobro da quantidade: {dobro}");
+        Console.WriteLine();
+
+        Console.WriteLine("Parâmetros duplicados:");
+
+        foreach (var parametro in args)
+        {
+            Console.WriteLine(parametro);
+            Console.WriteLine(parametro);
+        }
+    }
+}
